f6473c0ce201ca476a0ad3993ddb3c466d30b588
Om Kamath <omganesh402@gmail.com>
2024-03-26 18:46:10 +0530

    Added a bar for the user profiling. Magic

--- main.py
+++ main.py
@@ -13,54 +13,98 @@ from langchain_community.embeddings.sentence_transformer import (
 )
 import pandas as pd
 import API
+import ast
+import time
+
+
+
+#############################################
+# Setup for API and Database
 
 # create the open-source embedding function
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-
-
+# load the vectors for similarity search
 db2 = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
 
+# Fetching the csv for program description
+df = pd.read_csv("academic_routes_final.csv")
+
+# Gemini Pro API Key
 API_KEY = API.API_KEY
+
 # Creating an instance of the LangChain class
 llm = ChatGoogleGenerativeAI(model="gemini-pro",google_api_key=API_KEY)
-agent = create_csv_agent(llm,"academic_routes_final.csv",verbose=True)
+# agent = create_csv_agent(llm,"academic_routes_final.csv",verbose=True)
 
+###########################################
+# APP BEGINS HERE
 
 # Title of the app
 st.title("Career Navigator")
+st.header("Tell us about yourself")
 
 
-st.header("Ask about Educational Prospects")
-
+# User input
+user_profile = st.text_input("What are you currently pursuing?", "I am doing a degree in Computer Science.")
 course_recommendation = st.text_area("What are you looking out for?", "I like to code and work with data.")
 slider = st.slider("How many courses would you like to see?", 1, 10, 3)
 courses = db2.similarity_search(course_recommendation, k=slider)
+st.divider()
 
-
-st.write("Here are some courses that you might be interested in:")
-
-
+# Display the courses fetched using similarity search
+st.markdown("### Here are some courses that you might be interested in:")
 for course in courses:
     course_name = course.page_content.split(":")[0]
-    st.markdown(f'* {course_name}')
-
-for course in courses:
-    course_name = course.page_content.split(":")[0]
-    st.markdown(f'## What is covered in {course_name}?')
-    response = agent.run(f"Find the Description for Program Name:'{course_name}' in string format.")
-    st.markdown(f"<p>{response}</p>", unsafe_allow_html=True)
+    description = df[df['Program'] == course_name]['Description'].values[0]
+    st.markdown(f'##### {course_name}')
+    st.markdown(f'<p>{description}</p>', unsafe_allow_html=True)
 
 
-data = []
-# Create table using for loop in streamlit
-for course in courses:
-    course_name = course.page_content.split(":")[0]
-    subject_areas = agent.run(f"Find the Subject Areas for Program Name: '{course_name}' in string format.")
-    data.append({"Course Name": course_name, "Subject Areas": subject_areas})
+# Prompt Template
+full_template = """User Data: {user_profile}
 
-df = pd.DataFrame(data)
+User Request: {course_recommendation}
 
-st.table(df)
+Courses: {courses}
 
+Based on the information provided, analyse the user's profile into the following categories:
+1. Technical
+2. Creative
+3. Analytical
+
+You need to provide scores out of 10 for each of the categories as a array.
+
+For eg.
+[8, 5, 7]
+"""
+
+full_prompt = PromptTemplate.from_template(full_template)
+prompt = full_prompt.format(user_profile=user_profile, course_recommendation=course_recommendation, courses=courses)
+
+data = llm.invoke(prompt).content
+data = ast.literal_eval(data)
+
+
+
+technical_bar = st.progress(0, text="Technical")
+
+for percent_complete in range(data[0]*10):
+    time.sleep(0.01)
+    technical_bar.progress(percent_complete + 1, text="Technical")
+
+
+creative_bar = st.progress(0, text="Creative")
+
+for percent_complete in range(data[1]*10):
+    time.sleep(0.01)
+    creative_bar.progress(percent_complete + 1, text="Creative")
+
+
+
+analytical_bar = st.progress(0, text="Analytical")
+
+for percent_complete in range(data[2]*10):
+    time.sleep(0.01)
+    analytical_bar.progress(percent_complete + 1, text="Analytical")
 
